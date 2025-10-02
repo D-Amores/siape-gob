5a53588efc815cd598d11a655fd4c2e4808479cb
amores <amores.dev@outlook.com>
2025-10-02 13:45:32 -0600

    feat(api): define asset resource routes (index, store, update, destroy)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AssetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('assets', AssetController::class)->only(['index', 'store', 'update', 'destroy']);
