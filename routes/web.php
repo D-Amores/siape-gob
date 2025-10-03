0ac464d02ad15bc3151a3ef8f0ab52e2f25e8f6c
amores <amores.dev@outlook.com>
2025-10-03 11:20:27 -0600

    feat(api): define brand resource routes (index, store, update, destroy)

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\AssetController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\PersonnelController;
+use App\Http\Controllers\BrandController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('assets', AssetController::class)->only(['index', 'store', 'update', 'destroy']);
+Route::resource('brands', BrandController::class)->only(['index', 'store', 'update', 'destroy']);
+
 Route::resource('personnel', PersonnelController::class)->only([
     'index', 'store', 'update', 'destroy'
 ]);
