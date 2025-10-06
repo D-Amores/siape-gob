6647292fa01177a123148164f7b58b8e10679aba
amores <amores.dev@outlook.com>
2025-10-06 00:24:01 -0600

    feat(api): define personalAssetPending resource routes (index, store, update, destroy)

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\BrandController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PersonalAssetController;
+use App\Http\Controllers\PersonalAssetPendingController;
 use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
@@ -20,7 +21,7 @@ Route::resource('assets', AssetController::class)->only(['index', 'store', 'upda
 Route::resource('brands', BrandController::class)->only(['index', 'store', 'update', 'destroy']);
 Route::resource('categories', CategoryController::class)->only(['index', 'store', 'update', 'destroy']);
 Route::resource('personal-asset', PersonalAssetController::class)->only(['index', 'store', 'update', 'destroy']);
-
+Route::resource('personal-asset-pending', PersonalAssetPendingController::class)->only(['index', 'store', 'update', 'destroy']);
 
 //RUTA PARA EL CRUD DE PERSONAL
 Route::resource('personnel', PersonnelController::class)->only([
