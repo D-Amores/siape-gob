69e7be11fc6243a7dcb97b27380e9547be4507e1
JoseeMelk <moralescastellanos1234@gmail.com>
2025-10-09 15:19:47 -0600

    add(personnel): add datatable filter

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.layout')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('modernize/assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('cdn/buttons/2.4.2/css/buttons.dataTables.min.css') }}">
+
+@endsection
 @section('title', 'Panel de Administración')
-@section('subtitle', 'Gestión de usuarios y registros pendientes')
+@section('subtitle', 'Gestión de personal y usuarios')
 
 @section('actions')
     <button class="btn btn-primary w-md-auto me-2" id="btnAbrirCrearUsuario" data-bs-toggle="modal"
-        data-bs-target="#crearUsuarioModal">
-        <i class="bx bx-plus me-1"></i>
-        Crear Usuario
-    </button>
-    <button class="btn btn-outline-primary w-md-auto me-2" id="btnAbrirCrearUsuario" data-bs-toggle="modal"
-        data-bs-target="#crearPersonalModal">
+        data-bs-target="#personnelModalCreate">
         <i class="bx bx-plus me-1"></i>
         Registrar Personal
+    </button>
+    <button class="btn btn-outline-primary w-md-auto me-2" id="btnAbrirCrearUsuario" data-bs-toggle="modal"
+        data-bs-target="#userModalCreate">
+        <i class="bx bx-plus me-1"></i>
+        Crear Usuario
     </button>
 @endsection
 
@@ -27,9 +32,9 @@
                         <h4 class="mb-0">
                             Panel de Administración
                         </h4>
-                        <small class="text-muted">Gestión de usuarios y registros pendientes</small>
+                        <small class="text-muted">Gestión de users y registros personnel</small>
                     </div>
-                    <button class="btn btn-primary w-md-auto" id="btnAbrirCrearUsuario" data-bs-toggle="modal" data-bs-target="#crearUsuarioModal">
+                    <button class="btn btn-primary w-md-auto" id="btnAbrirCrearUsuario" data-bs-toggle="modal" data-bs-target="#userModalCreate">
                         <i class="bx bx-plus me-1"></i>
                         Crear Usuario
                     </button>
@@ -40,92 +45,91 @@
                 <ul class="nav nav-tabs gap-2" id="adminTabs" role="tablist">
                     <li class="nav-item" role="presentation">
                         <button class="nav-link border-3 border-primary border-bottom-0 rounded-top-2 rounded-bottom-0"
-                            id="pendientes-tab" data-bs-toggle="tab" data-bs-target="#pendientes" type="button"
-                            role="tab" aria-controls="pendientes" aria-selected="true">
-                            Registros Pendientes
+                            id="personnel-tab" data-bs-toggle="tab" data-bs-target="#personnel" type="button"
+                            role="tab" aria-controls="personnel" aria-selected="true">
+                            Personal
                         </button>
                     </li>
                     <li class="nav-item" role="presentation">
                         <button class="nav-link border-3 border-primary border-bottom-0 rounded-top-2 rounded-bottom-0"
-                            id="usuarios-tab" data-bs-toggle="tab" data-bs-target="#usuarios" type="button" role="tab"
-                            aria-controls="usuarios" aria-selected="false">
+                            id="users-tab" data-bs-toggle="tab" data-bs-target="#users" type="button" role="tab"
+                            aria-controls="users" aria-selected="false">
                             Usuarios
                         </button>
                     </li>
                 </ul>
                 <div class="tab-content card mb-3 rounded-top-0 border-primary border-3 shadow" id="adminTabsContent">
-                    <!-- Tabla Registros Pendientes -->
-                    <div class="tab-pane fade" id="pendientes" role="tabpanel" aria-labelledby="pendientes-tab">
-                        <div class="card-header">
-                            <h5 class="mb-0">
-                                <i class="bx bx-time-five me-2"></i>
-                                Registros Pendientes
-                            </h5>
+                    <!-- Tabla Registros Personnel -->
+                    <div class="tab-pane fade datatables" id="personnel" role="tabpanel" aria-labelledby="personnel-tab">
+                        <div class="card-header text-center">
+                            <h4 class="mb-0">
+                                <i class="bx bx-user"></i>
+                                Lista de Personal
+                            </h4>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body p-3">
                             <div class="table-responsive">
-                                <table class="table mb-0">
-                                    <thead>
+                                <table id="dataPersonnelTable" class="table text-nowrap table-bordered align-middle">
+                                    <thead class="text-dark fs-4">
                                         <tr>
                                             <th scope="col">#</th>
-                                            <th scope="col">First</th>
-                                            <th scope="col">Last</th>
-                                            <th scope="col">Handle</th>
-                                            <th scope="col">Actions</th>
+                                            <th scope="col">Nombre(s)</th>
+                                            <th scope="col">Teléfono</th>
+                                            <th scope="col">E-mail</th>
+                                            <th scope="col">Área</th>
+                                            <th scope="col">Estado</th>
+                                            <th scope="col">Acciones</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
                                             <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
+                                            <td>
+                                                <div class="d-flex align-items-center">
+                                                    <img src="{{ asset('modernize/assets/images/profile/user-6.jpg') }}" class="rounded-circle" width="40" height="40">
+                                                    <div class="ms-3">
+                                                        <h6 class="fs-4 fw-semibold mb-0">Christopher Jamil</h6>
+                                                        <span class="fw-normal">Morales Flores</span>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>9991112222</td>
+                                            <td>carlos.morales@example.com</td>
+                                            <td>Recursos Humanos</td>
+                                            <td>
+                                                <span class="badge bg-primary-subtle text-primary">activo</span>
+                                            </td>
                                             <td>
                                                 <div class="btn-group" role="group">
                                                     <button class="btn btn-sm btn-outline-success"><i
-                                                            class="bx bx-check"></i></button>
+                                                            class="bx bx-edit"></i></button>
                                                     <button class="btn btn-sm btn-outline-danger"><i
                                                             class="bx bx-trash"></i></button>
                                                 </div>
                                             </td>
                                         </tr>
+
                                         <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
+                                            <th scope="row">1</th>
                                             <td>
-                                                <div class="btn-group" role="group">
-                                                    <button class="btn btn-sm btn-outline-success"><i
-                                                            class="bx bx-check"></i></button>
-                                                    <button class="btn btn-sm btn-outline-danger"><i
-                                                            class="bx bx-trash"></i></button>
+                                                <div class="d-flex align-items-center">
+                                                    <img src="{{ asset('modernize/assets/images/profile/user-6.jpg') }}" class="rounded-circle" width="40" height="40">
+                                                    <div class="ms-3">
+                                                        <h6 class="fs-4 fw-semibold mb-0">Jose Jamil</h6>
+                                                        <span class="fw-normal">Martinez Morales</span>
+                                                    </div>
                                                 </div>
                                             </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Larry</td>
-                                            <td>the Bird</td>
-                                            <td>@twitter</td>
+                                            <td>9991112222</td>
+                                            <td>carlos.morales@example.com</td>
+                                            <td>Recursos Humanos</td>
                                             <td>
-                                                <div class="btn-group" role="group">
-                                                    <button class="btn btn-sm btn-outline-success"><i
-                                                            class="bx bx-check"></i></button>
-                                                    <button class="btn btn-sm btn-outline-danger"><i
-                                                            class="bx bx-trash"></i></button>
-                                                </div>
+                                                <span class="badge bg-primary-subtle text-primary">activo</span>
                                             </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Larry</td>
-                                            <td>the Bird</td>
-                                            <td>@twitter</td>
                                             <td>
                                                 <div class="btn-group" role="group">
                                                     <button class="btn btn-sm btn-outline-success"><i
-                                                            class="bx bx-check"></i></button>
+                                                            class="bx bx-edit"></i></button>
                                                     <button class="btn btn-sm btn-outline-danger"><i
                                                             class="bx bx-trash"></i></button>
                                                 </div>
@@ -139,8 +143,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Tabla Usuarios -->
-                    <div class="tab-pane fade" id="usuarios" role="tabpanel" aria-labelledby="usuarios-tab">
+                    <!-- Tabla users -->
+                    <div class="tab-pane fade" id="users" role="tabpanel" aria-labelledby="users-tab">
                         <div class="card-header">
                             <h5 class="mb-0">
                                 <i class="bx bx-users me-2"></i>
@@ -232,7 +236,7 @@
     </div>
 
         <!-- Modal Crear Personal -->
-<div class="modal fade" id="crearPersonalModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
+<div class="modal fade" id="personnelModalCreate" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
   <div class="modal-dialog modal-dialog-centered modal-lg">
     <div class="modal-content border border-primary border-2 shadow">
       <div class="modal-header bg-primary text-white">
@@ -313,7 +317,7 @@
 
 
     <!-- Modal Crear Usuario -->
-    <div class="modal fade" id="crearUsuarioModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal fade" id="userModalCreate" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
         <div class="modal-dialog modal-dialog-centered modal-lg"> <!-- Centrado y tamaño grande -->
             <div class="modal-content">
                 <div class="modal-header">
@@ -392,7 +396,7 @@
     </div>
 
     <!-- Modal Editar Usuario -->
-    <div class="modal fade" id="editarUsuarioModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal fade" id="userModalEdit" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -478,3 +482,14 @@
     const authUserId = {{ auth()->id() }};
 </script>
 @endsection --}}
+
+@section('scripts')
+    <script>
+        const languageDataTable = '{{ asset("cdn/datatables-language/es-MX.json") }}';
+    </script>
+    <script src="{{ asset('modernize/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('modernize/assets/js/datatable/datatable-advanced.init.js') }}"></script>
+    
+    <!-- Script table-config.js -->
+    <script src="{{ asset('js/admin/table-config.js') }}"></script>
+@endsection
