064523b5747b4d70629bb464d52ca4cd3b2af00c
JoseeMelk <moralescastellanos1234@gmail.com>
2025-10-16 15:49:34 -0600

    fix(assigner): move asset to assigner folder

--- app/Http/Requests/Assigner/AssetsApiRequest.php
+++ app/Http/Requests/Assigner/AssetsApiRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Assigner;
 
 use Illuminate\Foundation\Http\FormRequest;
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AssetController;
+use App\Http\Controllers\Assigner\AssetController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\PersonnelController;
 use App\Http\Controllers\Admin\UserController;
