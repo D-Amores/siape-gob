36880ebb7b073966baa8ff5f60f5ed7e7f7f30c9
JoseeMelk <moralescastellanos1234@gmail.com>
2025-10-23 10:30:15 -0600

    fix(layout): fix layout routes

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\Admin\PersonnelController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\AreaController;
 use App\Http\Controllers\Auth\LoginController;
-
 use App\Http\Controllers\PersonnelAssetController;
 use App\Http\Controllers\Assigner\PersonnelAssetPendingController;
 
@@ -21,7 +20,6 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
     // Logout route
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
@@ -35,12 +33,7 @@ Route::middleware('auth')->group(function () {
         Route::post('categories/api', [CategoryController::class, 'categoryApi']);
         Route::post('personnel-asset-pending/api', [PersonnelAssetPendingController::class, 'personnelAssetPendingApi']);
         Route::post('select-assets/api', [AssetController::class, 'selectAssetsApi']);
-        // Ruta para obtener assets para la tabla o detalles
         Route::post('assets/api', [AssetController::class, 'assetsApi'])->name('assets.api');
-
-        //RUTA QUE SE ELIMINARA YA QUE NO NOS SERVIRÁ
-        //Route::resource('personnel-asset', PersonnelAssetController::class)->only(['index', 'store', 'update', 'destroy']);
-        // Route::post('personnel-asset/api', [PersonnelAssetController::class, 'personnelAssetApi']);
     });
 
     Route::middleware('role:user')->group(function () {
@@ -51,19 +44,14 @@ Route::middleware('auth')->group(function () {
 
     Route::middleware('role:admin')->group(function () {
         // Admin routes
-        //RUTA PARA EL CRUD DE USUARIOS
         Route::resource('admin/users', UserController::class)->only([
             'index', 'show', 'store', 'update', 'destroy'
         ]);
         Route::post('admin/users/api', [UserController::class, 'userApi']);
-
-        //RUTA PARA EL CRUD DE AREAS
-        Route::post('admin/areas/api', [AreaController::class, 'areaApi']);
-
-        //RUTA PARA EL CRUD DE PERSONAL
         Route::resource('admin/personnel', PersonnelController::class)->only([
             'index', 'show', 'store', 'update', 'destroy'
         ]);
+        Route::post('admin/areas/api', [AreaController::class, 'areaApi']);
         Route::post('admin/personnel/api', [PersonnelController::class, 'personnelApi']);
     });
 });
