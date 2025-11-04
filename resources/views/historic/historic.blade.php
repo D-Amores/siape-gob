69e1e4f086e7f4755c3d15bfd8709670c550fa1e
JoseeMelk <moralescastellanos1234@gmail.com>
2025-11-03 18:55:56 -0600

    fix(historyTable): fix table

--- resources/views/historic/historic.blade.php
+++ resources/views/historic/historic.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.layout')
 
 @section('styles')
-    {{-- <link rel="stylesheet" href="{{ asset('modernize/assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('cdn/buttons/2.4.2/css/buttons.dataTables.min.css') }}">
 @endsection
 
@@ -13,39 +12,32 @@
     Aquí puedes ver tu historial de bienes.
 @endsection
 
-{{-- @section('actions')
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCategoryModal">
-        <i class="fas fa-plus-circle me-2"></i> Agregar Categoria
-    </button>
-@endsection --}}
-
 @section('content')
     <div class="container-fluid">
-        <div class="datatables">
-            <div class="card shadow-lg">
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table id="historic" class="table table-hover w-100 table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Activo</th>
-                                    <th scope="col">Marca</th>
-                                    <th scope="col">Categoría</th>
-                                    <th scope="col">Asignado Por</th>
-                                    <th scope="col">Asignado A</th>
-                                    <th scope="col">Fecha de Asignación</th>
-                                    <th scope="col">Fecha de Confirmación</th>
-                                    <th scope="col">Fecha de Desasignación</th>
-                                    <th scope="col">Estado</th>
-                                    <th scope="col">Acciones</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                            </tbody>
-                        </table>
-                    </div>
+        <div class="card shadow-lg">
+            <div class="card-body">
+                {{-- ✅ Hacemos la tabla 100% responsive con Bootstrap --}}
+                <div class="table-responsive">
+                    <table id="historic" class="table table-hover table-striped table-bordered align-middle text-center">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Activo</th>
+                                <th>Marca</th>
+                                <th>Categoría</th>
+                                <th>Asigna</th>
+                                <th>Asignado</th>
+                                <th>Asignación</th>
+                                <th>Confirmación</th>
+                                <th>Desasigna</th>
+                                <th>Estado</th>
+                                <th>Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- Aquí va el contenido dinámico --}}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -56,6 +48,7 @@
     <script>
         const vHistoricApi = `${BASE_URL}/historic/api`;
     </script>
+
     <script src="{{ asset('cdn/buttons/3.0.2/js/dataTables.buttons.min.js') }}"></script>
     <script src="{{ asset('cdn/buttons/3.0.2/js/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('cdn/buttons/3.0.2/js/buttons.print.min.js') }}"></script>
@@ -72,5 +65,4 @@
     <!-- Historic JS -->
     <script src="{{ asset('js/historic/historic-api.js') }}"></script>
     <script src="{{ asset('js/historic/historic.js') }}"></script>
-
 @endsection
