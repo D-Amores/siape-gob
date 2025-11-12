5b543bcbc6f6a5a0dd733ac1c3b4b52d23ebe582
JoseeMelk <moralescastellanos1234@gmail.com>
2025-11-12 11:08:09 -0600

    refactor(status): add field description to status and add status seeder

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -12,6 +12,7 @@ class Status extends Model
 
     protected $fillable = [
         'name',
+        'description',
     ];
 
     protected $table = 'statuses';
---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Status;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $statuses = [
+            /**
+             * Estados de activos
+             */
+            ['name' => 'NUEVO', 'description' => 'El activo es nuevo y no ha sido utilizado.'],
+            ['name' => 'DISPONIBLE', 'description' => 'El activo está disponible para su uso.'],
+            ['name' => 'EN MANTENIMIENTO', 'description' => 'El activo está actualmente en mantenimiento.'],
+            ['name' => 'FUERA DE SERVICIO', 'description' => 'El activo no está en condiciones de ser utilizado.'],
+            /**
+             * Estados de reportes de mantenimiento
+             */
+            ['name' => 'ABIERTO', 'description' => 'El seguimiento del mantenimiento está en proceso.'],
+            ['name' => 'EN SEGUIMIENTO', 'description' => 'El activo está en seguimiento.'],
+            ['name' => 'COMPLETADO', 'description' => 'El seguimiento del mantenimiento ha sido completado.'],
+        ];
+
+        foreach ($statuses as $statusData) {
+            Status::firstOrCreate(['name' => $statusData['name']], $statusData);
+        }
+    }
+}
